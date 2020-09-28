f026b50b151779ea18798a1a6a671a754ea757d6
Igor Rozum <rozumiq@gmail.com>
2020-09-27 21:26:40 -0400

    Add new link form ready

--- scraper/views.py
+++ scraper/views.py
@@ -1,9 +1,17 @@
-# from django.shortcuts import render
-from django.views import View, generic
+from django.views import generic
 from scraper.models import Link, Price
+from django.shortcuts import redirect
+from urllib.parse import urlparse
 
 
 class Index(generic.ListView):
     model = Link
     template_name = 'scraper/index.html'
-    # ordering = ['-date_generated']
+
+    def post(self, request, *args, **kwargs):
+        product = request.POST.get('product', '')
+        link = request.POST.get('link', '')
+        domain = urlparse(link).netloc
+        website = domain if domain else 'N/A'
+        Link(website_name=website, product_name=product, link=link).save()
+        return redirect('/')
